e267907e11cbfc6ffd66f4dc5f08ea180a18f53d
JustJabbaa <72550781+JustJabbaa@users.noreply.github.com>
2021-09-13 15:28:30 +0200

    123

--- Node.js
+++ Node.js
@@ -11,3 +11,7 @@ app.listen(port, () => {
 });
 const square = require('./square');
 console.log
+app.all('/secret', function(req, res, next) {
+    console.log('Accessing the secret section ...');
+    next(); // pass control to the next handler
+  });
