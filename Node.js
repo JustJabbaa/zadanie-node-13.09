64f59f73a74cb7847b7b14f3c52133d538d5b69e
JustJabbaa <72550781+JustJabbaa@users.noreply.github.com>
2021-09-13 15:32:14 +0200

    wiki

--- Node.js
+++ Node.js
@@ -15,3 +15,5 @@ app.all('/secret', function(req, res, next) {
     console.log('Accessing the secret section ...');
     next(); // pass control to the next handler
   });
+  const wiki = require("./wiki.js");
+  app.use("/wiki", wiki);
